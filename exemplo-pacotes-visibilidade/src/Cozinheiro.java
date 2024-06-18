2771a8134c817202db292b89f6cd8dc6fd76fef2
LeoHossary <leonardohossary@gmail.com>
2024-06-18 09:46:49 -0400

    feat: add classes do projeto

---
+++ exemplo-pacotes-visibilidade/src/Cozinheiro.java
@@ -0,0 +1,46 @@
+public class Cozinheiro {
+  public void adicionarLancheNoBalcao() {
+    System.out.println("ADICONANDO LANCHE NATURAL NO BALCÃO");
+  }
+  public void adicionarSucoNoBalcao() {
+    System.out.println("ADICIONANDO SUCO NO BALCÃO");
+  }
+  public void adicionarComboNoBalcao() {
+    adicionarLancheNoBalcao();
+    adicionarSucoNoBalcao();
+  }
+  public void prepararLanche() {
+    System.out.println("PREPARANDO LANCHE");
+  }
+  public void prepararVitamina() {
+    System.out.println("PREPARANDO VITAMINA");
+  }
+  public void prepararCombo() {
+    prepararLanche();
+    prepararVitamina();
+  }
+  public void selecionarIngedientesLanche() {
+    System.out.println("SELECIONANDO O PÃO, SALADA, CARNE E QUEIJO");
+  }
+  public void selecionarIngredientesVitamina() {
+    System.out.println("SELECIONANDO FRUTA, LEITE E AÇUCAR");
+  }
+  public void lavarIngredientes() {
+    System.out.println("LAVANDO INGREDIENTES");
+  }
+  public void baterVitaminaLiquidificador() {
+    System.out.println("BATENDO VITAMINA NO LIQUIDIFICADOR");
+  }
+  public void fritarIngredientesLanche() {
+    System.out.println("FRITANDO A CARNE E OVO NA CHAPA");
+  }
+  public void pedirParaTrocarGas(Atendente meuAmigo) {
+    meuAmigo.trocarGas();
+  }
+  public void pedirParaTrocarGas(Almoxarife meuAmigo) {
+    meuAmigo.trocarGas();
+  }
+  public void pedirIngredientes(Almoxarife almoxarife) {
+    almoxarife.entregarIngredientes();
+  }
+}
